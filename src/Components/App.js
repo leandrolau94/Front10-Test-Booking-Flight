e6f854582b38434c2f343c627b2d78e2bbd3eefd
leandrolau94 <l1d9l9a4@gmail.com>
2022-08-25 08:08:55 +0200

    proving data fetching with another web url public

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,13 @@
-import React, { useState } from "react";
-// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import BookingFlight from "./BookingFlight/BookingFlight";
 // Setting up router
 // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 //creating context
 export const BookFlightContext = React.createContext(null);
 
-// const baseURL = `https://www.kayak.com/`;
+const baseURL = `https://www.kayak.com/`; // unsuccessfull data fetching here.
+// const baseURL = "https://jsonplaceholder.typicode.com/posts/1";//successfull data fetching here.
 
 const initialBookState = {
   flightType: "",
@@ -23,28 +24,28 @@ const App = () => {
 
   const [flightData, setFlightData] = useState([]);
 
-  // const getFlightData = () => {
-  //   axios
-  //     .get(`${baseURL}`, {
-  //       headers: {
-  //         "Access-Control-Allow-Origin": "*",
-  //         // origin: "x-requested-with",
-  //         "Access-Control-Allow-Headers": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  //         // "Content-Type": "application/json",
-  //       },
-  //     })
-  //     .then((res) => {
-  //       console.log(res);
-  //       setFlightData(res.data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
+  const getFlightData = () => {
+    axios
+      .get(`${baseURL}`, {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          // origin: "x-requested-with",
+          "Access-Control-Allow-Headers": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+          // "Content-Type": "application/json",
+        },
+      })
+      .then((res) => {
+        console.log(res);
+        setFlightData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  // useEffect(() => {
-  //   getFlightData();
-  // }, []);
+  useEffect(() => {
+    getFlightData();
+  }, []);
 
   return (
     <React.Fragment>
